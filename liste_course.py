ab3bfe74050aa6f3b0b29d24fa4c45b96d65d000
Raphael Ollier <raphael.ollier7@gmail.com>
2024-06-11 10:16:05 +0200

    Ajout de docstrings sur les 3 premières méthodes, __init__, __str__ et __repr__ (Raphael).

--- liste_course.py
+++ liste_course.py
@@ -3,15 +3,36 @@ from tinydb import TinyDB
 from pathlib import Path
 
 class Produit:
+    # On initialise une instance de TinyDB en attribut de classe
     DB = TinyDB(Path(__file__).resolve().parent / 'data.json', indent=4)
-    def __init__(self, nom: str, quantite = 1):
+
+    def __init__(self, nom: str, quantite = "1"):
+        """Méthode d'initialisation de nos instances de Produit.
+
+        Args:
+            nom (str): le nom du produit.
+            quantite (str, optional): la quantité souhaité pour ce produit. Defaults to "1".
+        """
+
         self.nom = nom.capitalize()
         self.quantite = quantite
     
     def __str__(self):
+        """Méthode pour mettre en forme nos instances lorsqu'elle sont print ou convertis en str().
+
+        Returns:
+            (str) : le nom de l'instance.
+        """
+
         return f"{self.nom}"
 
     def __repr__(self):
+        """Méthode de représentation de nos intances. Montre comment recréer l'instance à partir de la classe.
+
+        Returns:
+            (str) : comment re-créer l'instance à partir de la classe.
+        """
+
         return f"Produit({self.nom}, {self.quantite})"
     
 if __name__ == '__main__':
