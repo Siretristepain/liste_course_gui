e63ecd7dc142fe9c986036516c6f10c187a6c992
Raphael Ollier <raphael.ollier7@gmail.com>
2024-06-11 11:30:30 +0200

    Création des méthodes _write_items() et _get_items() (Raphael)
    La méthode _write_items() permet de stocker les attributs d'une instances (son nom et sa quantité) dans le bdd JSON.
    La méthode _get_items() permet de récupérer tous ces éléments dans la bdd JSON sous forme de liste de tuple de la forme [(nom, quantite)]

--- liste_course.py
+++ liste_course.py
@@ -1,6 +1,7 @@
 from PySide2 import QtWidgets
-from tinydb import TinyDB
+from tinydb import TinyDB, where
 from pathlib import Path
+from typing import List
 
 class Produit:
     # On initialise une instance de TinyDB en attribut de classe
@@ -35,5 +36,34 @@ class Produit:
 
         return f"Produit({self.nom}, {self.quantite})"
     
+    def _write_items(self):
+        """Méthode privée utilisée pour ajouter un produit et sa quantité dans la bdd (-> fichier JSON).
+        """
+
+        Produit.DB.insert({'nom' : self.nom, 'quantite' : self.quantite})
+
+    def _get_items(self) -> List[tuple]:
+        """Méthode privée utilisée pour récupérer tous les éléments de notre bdd sous forme de liste de tuple (nom, quantite).
+
+        Returns:
+            List[tuple]: [(nom, quantite)]
+        """
+        # On récupère tous les éléments de notre bdd dans une liste
+        all_items = [(x['nom'], x['quantite']) for x in Produit.DB]
+
+        # Si la liste est vide, on retourne [("",0)] pour être cohérent avec notre docstring
+        if bool(all_items) == False:
+            return [("",0)]
+        
+        return all_items
+
+    
 if __name__ == '__main__':
     banane = Produit("banane")
+    print(banane)
+
+    # Ajout de banane dans la bdd
+    # banane._write_items()
+
+    # Récupération des items de la bdd
+    print(banane._get_items())
