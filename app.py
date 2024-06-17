30517fb68126cd0f3d87f251561d78913c353d2f
Raphael Ollier <raphael.ollier7@gmail.com>
2024-06-17 14:54:19 +0200

    Mise en forme de l'interface visuelle (Raphael)
    Visuellement, l'application fonctionne. On retrouve la LineEdit, le bouton d'ajout.
    La listWidget où apparaitront les produits.
    On a le bouton 'retirer' et 'clean' côte à côte, ce qui a nécessité un deuxième layout.

--- app.py
+++ app.py
@@ -4,7 +4,29 @@ class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Liste de course")
+        self.setup_ui()
         self.resize(600,500)
+
+    def setup_ui(self):
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.lineEdit = QtWidgets.QLineEdit()
+        self.button_add = QtWidgets.QPushButton("Ajouter")
+        self.listWidget = QtWidgets.QListWidget()
+        self.button_remove = QtWidgets.QPushButton("Retirer")
+        self.button_clean = QtWidgets.QPushButton("Nettoyer")
+
+        # Création du sous-layout pour les boutons 'Retirer' et 'Nettoyer'
+        self.child_layout = QtWidgets.QHBoxLayout()
+        self.child_layout.addWidget(self.button_remove)
+        self.child_layout.addWidget(self.button_clean)
+
+        # On ajoute les widgets au layout
+        self.layout.addWidget(self.lineEdit)
+        self.layout.addWidget(self.button_add)
+        self.layout.addWidget(self.listWidget)
+        self.layout.addLayout(self.child_layout)
+        # self.layout.addWidget(self.button_remove)
+        # self.layout.addWidget(self.button_clean)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
